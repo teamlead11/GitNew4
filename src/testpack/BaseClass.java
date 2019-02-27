8e7a7e00b69857826f77a2036a89d8f30a9a6b3c
nirmalkumarnk3 <48005515+nirmalkumarnk3@users.noreply.github.com>
2019-02-27 13:10:37 +0530

    Revert "Rajesh"

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -1,45 +1,10 @@
 package testpack;
 
-import java.util.Scanner;
-
 public class BaseClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is my modified base class by RAJESH KANNA");
-		BaseClass rajesh=new BaseClass();
-		rajesh.arraySortByRajesh();
+		System.out.println("this is my modified base class");
 	}
-public void arraySortByRajesh()
-{
-	 int n, temp;
-     Scanner s = new Scanner(System.in);
-     System.out.print("Enter no. of Integer Value you want in array:");
-     n = s.nextInt();
-     int a[] = new int[n];
-     System.out.println("Enter all the Integer Values:");
-     for (int i = 0; i < n; i++) 
-     {
-         a[i] = s.nextInt();
-     }
-     for (int i = 0; i < n; i++) 
-     {
-         for (int j = i + 1; j < n; j++) 
-         {
-             if (a[i] > a[j]) 
-             {
-                 temp = a[i];
-                 a[i] = a[j];
-                 a[j] = temp;
-             }
-         }
-     }
-     System.out.print("Ascending Order:");
-     for (int i = 0; i < n - 1; i++) 
-     {
-         System.out.print(a[i] + ",");
-     }
-     System.out.print(a[n - 1]);
- }
 
 }
