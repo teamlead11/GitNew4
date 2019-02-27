115d1a705ceb377749a77632bf1c6e849773c2fb
Ragu20128 <raguramu20128@gmail.com>
2019-02-27 12:46:47 +0530

    Array completed

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -2,9 +2,34 @@ package testpack;
 
 public class BaseClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args){
+	
+	BaseClass s = new BaseClass();
+	s.arrayvisalvani();
+			
+	}
+
+		void arrayvisalvani() {
+		
 		System.out.println("this is my modified base class");
+		
+		int a[] = new int[5];
+a[0]=1;
+a[1]=12;
+a[2]=15;
+a[3]=19;
+a[4]=100;
+
+		for (int i = 0; i <= a.length; i++) {
+								
+			System.out.println(a[i]);
+		}
+		
+		
+		
+		
+		
 	}
 
 }
+
