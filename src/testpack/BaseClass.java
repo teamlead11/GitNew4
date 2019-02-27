23b54a0fda0388520937a9ce70f4ac1c06b0033d
VigneshMariyappan7007 <vignesh.mariyappan6161@gmail.com>
2019-02-27 13:05:41 +0530

    Project done

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -1,10 +1,42 @@
 package testpack;
 
-public class BaseClass {
+import java.util.Scanner;
 
+public class BaseClass {
+	public void arraySortByVignesh() {
+		
+		System.out.println("Enter 5 Values : ");
+		Scanner sc=new Scanner(System.in);
+		
+	int a[]=new int[5];
+	int temp;
+	for(int i=0;i<5;i++)
+	{
+		a[i]=sc.nextInt();
+	}
+	System.out.println("Ascending Order :");
+	for(int i=0;i<5;i++)
+	{
+		for(int j=i+1;j<5;j++)
+		
+		if(a[i]>a[j])
+		{
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+		
+		}
+	}
+	for(int i=0;i<5;i++)
+	{
+		System.out.println(a[i]);
+	}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		BaseClass Vikki =new BaseClass();
 		System.out.println("this is my modified base class");
+		Vikki.arraySortByVignesh();
 	}
 
 }
