844d4b8e13faf4c5a377306f4e93f3fee3c5927d
nirmalkumarnk3 <kumarkvs33@gmail.com>
2019-02-27 21:34:21 +0530

    Base class updated

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -1,10 +1,45 @@
-package testpack;
+	package testpack;
 
-public class BaseClass {
+	import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("this is my modified base class");
+	public class BaseClass {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			System.out.println("NIRMAL BASE CLASS");
+			BaseClass base=new BaseClass();
+			base.sortingArray();
+		}
+	public void sortingArray()
+	{
+		 int n, temp;
+	     Scanner s = new Scanner(System.in);
+	     System.out.print("Enter no. of Integer Value you want in array:");
+	     n = s.nextInt();
+	     int a[] = new int[n];
+	     System.out.println("Enter all the Integer Values:");
+	     for (int i = 0; i < n; i++) 
+	     {
+	         a[i] = s.nextInt();
+	     }
+	     for (int i = 0; i < n; i++) 
+	     {
+	         for (int j = i + 1; j < n; j++) 
+	         {
+	             if (a[i] > a[j]) 
+	             {
+	                 temp = a[i];
+	                 a[i] = a[j];
+	                 a[j] = temp;
+	             }
+	         }
+	     }
+	     System.out.print("Ascending Order:");
+	     for (int i = 0; i < n - 1; i++) 
+	     {
+	         System.out.print(a[i] + ",");
+	     }
+	     System.out.print(a[n - 1]);
+	 }
+
 	}
-
-}
