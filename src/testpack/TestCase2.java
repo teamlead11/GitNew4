0f09ecd6a9fbdc0d4dd1bfed7f32ef5dbfbca75d
teamlead11 <automationlead11@gmail.com>
2019-02-27 11:03:52 +0530

    modified base on review comments

--- src/testpack/TestCase2.java
+++ src/testpack/TestCase2.java
@@ -4,7 +4,7 @@ public class TestCase2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is my test case2");
+		System.out.println("this is my modified test case2");
 	}
 
 }
