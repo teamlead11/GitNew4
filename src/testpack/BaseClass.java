1e6b2b54d6c979dc43a3ec86729cecdfc2cf9e1b
Savithrirajasekar <savithrirajasekhar0@gmail.com>
2019-02-28 02:06:26 -0500

    array

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -1,10 +1,46 @@
 package testpack;
 
-public class BaseClass {
+import java.util.Arrays;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("this is my modified base class");
+public class BaseClass {
+	
+	
+	int[] num;
+	int n;
+	
+	
+	void array() {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Sort an array");
+		
+		System.out.println("Enter values");
+		n= Integer.parseInt(scan.nextLine());
+		
+		System.out.println("Enter the numbers");
+		num = new int[n];
+		
+		for(int i=0;i<n;i++) {
+			num[i]=Integer.parseInt(scan.nextLine());
+		}
+	}	
+		void sort() {
+			Arrays.sort(num);
+			
+			System.out.println("\n Ascending order");
+			
+			for(int i=0; i<n; i++) {
+				System.out.println(" "  + num[i]);
+		}
 	}
 
+public static void main(String[] args) {
+	
+	BaseClass array1 = new BaseClass();
+	array1.array();
+	array1.sort();
+	
+	}
+	
 }
