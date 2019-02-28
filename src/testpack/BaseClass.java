41ff2d5632343dc0bb6702564e68b84633876d36
Ragu20128 <raguramu20128@gmail.com>
2019-02-28 11:27:03 +0530

    If condition

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -2,34 +2,53 @@ package testpack;
 
 public class BaseClass {
 
-	public static void main(String[] args){
-	
-	BaseClass s = new BaseClass();
-	s.arrayvisalvani();
-			
-	}
-
-		void arrayvisalvani() {
-		
-		System.out.println("this is my modified base class");
-		
-		int a[] = new int[5];
-a[0]=1;
-a[1]=12;
-a[2]=15;
-a[3]=19;
-a[4]=100;
-
-		for (int i = 0; i <= a.length; i++) {
-								
-			System.out.println(a[i]);
+	 public static void main(String[] args) {
+			//String S="Raghav";
+			//int i= S.length();
+			//System.out.println(i);
+			//for (int J=0;J<S.length();J++) {
+				//if(S.charAt(J)=='a') {
+					//System.out.println(J);
+			//	}
+			//}
+				 
+				 //Exit (Need to check with positive and negative values
+				 //for (char i='A';i<10;i++) {
+					 //if(i=='A') {
+					//	 System.exit('g');
+					// }
+					// System.out.println(i);
+				 //}
+				 //System.out.println("END");
+				 int arr[] = {1, 2, 3, 4, 5, 6, 7, 8}; 
+				  	       
+				 for (int i = 0; i < arr.length; i++) 
+			        { 
+			            if (arr[i] >= 5) 
+			            { 
+			                System.out.println("exit..."); 
+			  
+			                // Terminate JVM 
+			                System.exit(41); 
+			            } 
+			            else
+			                System.out.println("arr["+i+"] = " + 
+			                                  arr[i]); 
+			        } 
+			        System.out.println("End of Program"); 
+		int roll =2;
+		switch (roll) {
+		case 1:
+			System.out.println("jeeva");
+			break;
+		case 2:
+			System.out.println("Muthu");
+			break;
+		case 3:
+			System.out.println("Raghav");
+			break;
 		}
-		
-		
-		
-		
-		
-	}
-
-}
-
+			 }
+			 }
+			 
+			 
