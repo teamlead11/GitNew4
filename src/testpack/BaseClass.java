d76452ed345be631253fde43b6efc62b36353738
naveenkumar15 <navinag63@gmail.com>
2019-03-01 00:38:01 +0530

    created a new method in base class

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -5,6 +5,17 @@ public class BaseClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my modified base class");
-	}
 
+	}
+	
+public void arraynaveen() {
+	// TODO Auto-generated method stub
+
+	      int[] age = {12, 4, 5, 2, 5};
+
+	      for (int i = 0; i < 5; ++i) {
+	         System.out.println("Element at index " + i +": " + age[i]);
+	      }
+	   }
 }
+
