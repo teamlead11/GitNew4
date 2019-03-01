f42704ee3f1ac1444670a7d0806ace3aa6cc9791
Savithrirajasekar <savithrirajasekhar0@gmail.com>
2019-03-01 01:11:26 -0500

    arraysort

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -1,10 +1,39 @@
 package testpack;
 
-public class BaseClass {
+import java.util.Arrays;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("My name is Savi");
+import javax.print.attribute.IntegerSyntax;
+
+public class BaseClass {
+	int num[];
+	int n;
+	void ArrayMethodbySavi() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Sorting the array");
+		System.out.println("Enter the values");
+		n=Integer.parseInt(sc.nextLine());
+		System.out.println("enter the numbers");
+		num=new int[n];
+		for(int i=0;i<n;i++) {
+			num[i]=Integer.parseInt(sc.nextLine());
+		}
 	}
+		void sort() {
+			Arrays.sort(num);
+			System.out.println("\n Ascending order");
+			for(int i=0;i<n;i++) {
+				System.out.println(" " +num[i]);
+				
+				
+			}
+		}
+	
+public static void main(String[] args) {
+	BaseClass base = new BaseClass();
+	base.ArrayMethodbySavi();
+	base.sort();
+}
+
 
 }
